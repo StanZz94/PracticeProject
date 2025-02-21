22d8154b6fd60c4ae691af3fb09d6b9039e8873a
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-02-21 23:41:42 +0200

    add loder() to EventsPage route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,18 @@ const router = createBrowserRouter([
       { index: true, element: <HomePage /> },
       {
         path: 'events', element: <EventsRootLayout />, children: [
-          { index: true, element: <EventsPage /> },
+          {
+            index: true, element: <EventsPage />, loader: async () => {
+              const response = await fetch('http://localhost:8080/events');
+
+              if (!response.ok) {
+                // set arror
+              } else {
+                const resData = await response.json();
+                return resData.events
+              }
+            }
+          },
           { path: 'new', element: <NewEventPage /> },
           { path: ':eventId', element: <EventDetails /> },
           { path: ':eventId/edit', element: <EditEventPage /> },
